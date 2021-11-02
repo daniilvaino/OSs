26ddb03db982e57120658b33407ec4bb9861b588
daniilvaino <daniil.vaino@gmail.com>
2021-11-02 05:19:27 +0300

    DE practicum

--- DE/program/Properties/Resources.cs
+++ DE/program/Properties/Resources.cs
@@ -1,9 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: ComputationalPractice.Properties.Resources
-// Assembly: ComputationalPractice, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 82529BA9-080A-4E86-A8A8-FD182D48B489
-// Assembly location: C:\Users\Lenovo\AppData\Local\Apps\2.0\K37TJG8Z.6GB\5HXM0QQ7.WLA\comp..tion_8f38878aa02c0079_0001.0000_e8ad7f6c69bbf218\ComputationalPractice.exe
-
+﻿
 using System.CodeDom.Compiler;
 using System.ComponentModel;
 using System.Diagnostics;
--- DE/program/src/NumericalMethod.cs
+++ DE/program/src/NumericalMethod.cs
@@ -1,10 +1,4 @@
-﻿// Decompiled with JetBrains decompiler
-// Type: ComputationalPractice.src.NumericalMethod
-// Assembly: ComputationalPractice, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
-// MVID: 82529BA9-080A-4E86-A8A8-FD182D48B489
-// Assembly location: C:\Users\Lenovo\AppData\Local\Apps\2.0\K37TJG8Z.6GB\5HXM0QQ7.WLA\comp..tion_8f38878aa02c0079_0001.0000_e8ad7f6c69bbf218\ComputationalPractice.exe
-
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace ComputationalPractice.src
 {
